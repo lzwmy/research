d22b3947d8cc60f44001ec80cfb68013675b7a52
lzw <885615757@qq.com>
2019-09-02 19:40:12 +0800

    添加科研课题页面

--- src/pages/index/packages/researchTask/index.vue
+++ src/pages/index/packages/researchTask/index.vue
@@ -4,9 +4,10 @@
         <div class="cloud-search flex-end-center">
             <el-button icon="el-icon-plus" type="primary">创建课题</el-button>
         </div>
-        <ul>
-            <li>
-
+        <ul class="flex-start-center">
+            <li v-for="(item, index) in 8" :key="index">
+                <div class="card_body"></div>
+                <div class="card_foot"></div>
             </li>
         </ul>
     </div>
@@ -31,6 +32,23 @@ export default {
     }
 }
 </script>
-<style lang="less">
-
+<style lang="less" scoped>
+    ul {
+        flex-wrap: wrap;
+        li {
+            box-shadow:0px 4px 10px rgba(0,0,0,0.16);
+            width: 285px;
+            margin: 20px 20px 0 0;
+            &:nth-child(4n) {
+                margin-right: 0;
+            }
+            .card_body {
+                height: 154px;
+            }
+            .card_foot {
+                height: 74px;
+                background-color: #fff;
+            }
+        }
+    }
 </style>
