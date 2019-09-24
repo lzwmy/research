bc1b7343c3aa58555cfa21712320bf5ebbde6ffb
lzw <885615757@qq.com>
2019-09-24 17:49:10 +0800

    修改创建科研项目接口

--- config/serverconfig.js
+++ config/serverconfig.js
@@ -6,5 +6,5 @@ module.exports = {
   //项目接口baseURL
   // baseURL: "http://120.77.199.11:8800/research/",
   // baseURL: "http://192.168.1.86:8080/research/",
-  baseURL: "http://39.108.238.209:8805/research/",
+  baseURL: "http://39.108.238.209:8806/research/",
 };
