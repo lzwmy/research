a708ad484501d81f9233a25ee086b0fdba3e68a4
Aiarron <1136315144@qq.com>
2019-11-09 17:53:21 +0800

    解决样式丢失问题

--- src/pages/index/packages/research/researchTarget/CRFConfig/display/displayPortion.vue
+++ src/pages/index/packages/research/researchTarget/CRFConfig/display/displayPortion.vue
@@ -328,7 +328,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="less" scoped>
+<style lang="less">
   .displayPortion_title{
     width: 100%;
     font-size: 14px;
