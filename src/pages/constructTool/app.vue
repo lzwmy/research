22424fcdc755c82922c19f500e89c0ac6a26cc8a
Aiarron <1136315144@qq.com>
2019-12-02 09:44:25 +0800

    2.1.3 代码合并

---
+++ src/pages/constructTool/app.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="construct_tool">
+      <div class="header">
+        <div class="header-box">
+
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "app"
+    }
+</script>
+
+<style scoped>
+.construct_tool .header {
+  background-color: #090e40;
+  height: 60px;
+}
+.construct_tool .header .header-box{
+  width: 1300px;
+  margin: 0 auto;
+  height: 100%;
+  overflow: auto;
+}
+</style>
