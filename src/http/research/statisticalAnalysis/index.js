c858b29219ddb0f5784ff6aad2358df8fb20f8b5
lzw <885615757@qq.com>
2019-12-03 18:05:33 +0800

    TASK_804 【专病v2.1.4】登录功能改造成使用Redis缓存验证用户

--- src/http/research/statisticalAnalysis/index.js
+++ src/http/research/statisticalAnalysis/index.js
@@ -5,11 +5,21 @@ const vm = new Vue();
 const url = {
     //获取分组
     getGroup: '/subject/analysis/group/list',
+    //获取指标列表
+    getTargetList: '/subject/analysis/variable/list',
+    //获取统计数据
+    getTanalysisData: '/subject/analysis/item/statistic',
 };
 
 const http = {
     statisticalAnalysisGroup (params) {
         return vm.$post(url.getGroup, params, true);
+    },
+    statisticalAnalysisTargetList (params) {
+        return vm.$post(url.getTargetList, params, true);
+    },
+    statisticalAnalysisTanalysisData (params) {
+        return vm.$post(url.getTanalysisData, params, false);
     },
 };
 
